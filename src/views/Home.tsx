284695e9a9095fa0376d2ff6d6565c82b1a59ab3
eemiljka <eemiljka@metropolia.fi>
2024-02-02 10:10:47 +0200

    class break commit

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,7 @@
-import {useState} from 'react';
 import {MediaItem} from '../types/DBTypes';
 import MediaRow from '../components/MediaRow';
-import SingleView from '../components/SingleView';
 
 const Home = () => {
-  const [selectedItem, setSelectedItem] = useState<MediaItem | undefined>();
   const mediaArray: MediaItem[] = [
     {
       media_id: 8,
@@ -45,9 +42,6 @@ const Home = () => {
 
   return (
     <>
-      {selectedItem && (
-        <SingleView item={selectedItem} setSelectedItem={setSelectedItem} />
-      )}
       <h2>My Media</h2>
       <table>
         <thead>
@@ -62,11 +56,7 @@ const Home = () => {
         </thead>
         <tbody>
           {mediaArray.map((item) => (
-            <MediaRow
-              key={item.media_id}
-              item={item}
-              setSelectedItem={setSelectedItem}
-            />
+            <MediaRow key={item.media_id} item={item} />
           ))}
         </tbody>
       </table>
